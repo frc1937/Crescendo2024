fb576c3c5ebe795ee528d834f9478621bd94e6bd
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-01-25 21:28:14 +0200

    Update VisionDrive.java

--- src/main/java/frc/robot/commands/VisionDrive.java
+++ src/main/java/frc/robot/commands/VisionDrive.java
@@ -7,8 +7,6 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Swerve;
 
-
-
 public class VisionDrive extends Command {
     
     private final Swerve swerve;
