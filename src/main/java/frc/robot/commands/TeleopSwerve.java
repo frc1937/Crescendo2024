5c5305cad1dff2e52200fffddbe4d09b55d94381
Wihy <105944317+itsWihy@users.noreply.github.com>
2024-01-31 11:51:55 +0200

    fix!: change loop to close

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -40,7 +40,7 @@ public class TeleopSwerve extends Command {
                 new Translation2d(translationValue, strafeValue).times(Constants.Swerve.MAX_SPEED),
                 rotationValue * Constants.Swerve.MAX_ANGULAR_VELOCITY,
                 !robotCentricSup.getAsBoolean(),
-                true
+                false
         );
     }
 }
