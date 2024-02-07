7bcf944731a98ecb5777ed21200321e14c964a00
Wihy <105944317+itsWihy@users.noreply.github.com>
2024-02-07 19:03:16 +0200

    fix: flywheel no longer uses PID

--- src/main/java/frc/robot/commands/ChaseTagCommand.java
+++ src/main/java/frc/robot/commands/ChaseTagCommand.java
@@ -33,8 +33,6 @@ public class ChaseTagCommand extends Command {
         driveController.getThetaController().reset(robotPose.getRotation().getRadians());
         driveController.getXController().reset();
         driveController.getYController().reset();
-
-        driveController.setEnabled();
     }
 
     @Override
