3acb6c15a257b827208a665c34a39395a9258690
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-02-09 11:58:38 +0200

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,17 +4,59 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
-import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import frc.lib.util.COTSFalconSwerveConstants;
 import frc.lib.util.SwerveModuleConstants;
 
 public final class Constants {
-    public static final double stickDeadband = 0.1;
+    /**
+     * Once how much time, in seconds, to run the infrequent periodic procedure
+     */
+    public static final double INFREQUENT_PERIODIC_PERIOD = 0.1;
+    public static final double STICK_DEADBAND = 0.1;
+
+    public static final class ChaseTagPIDConstants {
+        public static final TrapezoidProfile.Constraints OMEGA_CONSTRAINTS = new TrapezoidProfile.Constraints(8, 8);
+        public static final PIDController X_CONTROLLER = new PIDController(2, 0, 0);
+        public static final PIDController Y_CONTROLLER = new PIDController(2, 0, 0);
+        public static final ProfiledPIDController OMEGA_CONTROLLER = new ProfiledPIDController(4, 0, 0, OMEGA_CONSTRAINTS);
+
+        static {
+            X_CONTROLLER.setTolerance(0.2);
+            Y_CONTROLLER.setTolerance(0.2);
+            OMEGA_CONTROLLER.setTolerance(Units.degreesToRadians(3));
+            OMEGA_CONTROLLER.enableContinuousInput(-Math.PI, Math.PI);
+        }
+    }
+
+    public static final class Transforms {
+        /**
+         * Physical location of the camera on the robot, relative to the center of the robot. NEEDS TUNING
+         */
+        public static final Transform3d
+                CAMERA_TO_ROBOT = new Transform3d(new Translation3d(0.36, 0.39, 0.41), new Rotation3d()),
+                ROBOT_TO_CAMERA = CAMERA_TO_ROBOT.inverse(),
+                ROBOT_TO_PIVOT = new Transform3d(new Translation3d(-0.4, 0.1, -0.44), new Rotation3d());
+    }
 
     public static class VisionConstants {
-
+        public static final String CAMERA_NAME = "Photon1937";
+        public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT =
+                AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+        
         /**
          * Physical location of the camera on the robot, relative to the center of the robot. NEEDS TUNING
          */
@@ -29,24 +71,70 @@ public final class Constants {
 
         public static final int INTAKE_MOTOR_ID = 10; // JUST A PLACEHOLDER
     }
-    public static final class ShooterConstants {
-        public static final int
-                FLYWHEEL_LEFT_ID = 20,
-                FLYWHEEL_RIGHT_ID = 21,
-                PIVOT_ID = 23,
-                KICKER_ID = 22;
+    public static class IntakeConstants {
+        public static final int INTAKE_MOTOR_ID = 13;
+        public static final double INTAKE_SPEED = 0.8;
+    }
 
-        public static final double
-                PIVOT_KP = 0,
-                PIVOT_KI = 0,
-                PIVOT_KD = 0,
-                FLYWHEEL_KP = 0,
-                FLYWHEEL_KI = 0,
-                FLYWHEEL_KD = 0;
+    public static final class ShootingConstants {
+        /**
+         * This table maps theoretical (quasi) shooter slopes to shooter orientations that actually achieve
+         * the desired results, based on calibration and experimentation.
+         * <p>
+         * To obtain the samples, place the robot at some distance from the target, record the theoretical
+         * target slope given by the program as a key, and shoot with an arbitrary angle value. Shoot at an
+         * arbitrary angle and adjust it repeatedly until the robot consistently scores with the current slope(i.e.
+         * from its current position). Then move the robot to a different position and repeat until the table is
+         * complete. A good initial angle is the arc-tangent of the theoretical slope.
+         */
+        public static final InterpolatingTreeMap<Double, Rotation2d> SLOPE_TO_SHOOTER_ROTATION_MAP = new InterpolatingTreeMap<Double, Rotation2d>(InverseInterpolator.forDouble(), Rotation2d::interpolate);
+
+        static {
+            // TODO Currently, the values here are the initial guesses and are yet
+            // to be calibrated
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(0.0, Rotation2d.fromDegrees(0));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(0.2, Rotation2d.fromDegrees(11));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(0.4, Rotation2d.fromDegrees(21));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(0.6, Rotation2d.fromDegrees(30));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(0.8, Rotation2d.fromDegrees(38));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(1.0, Rotation2d.fromDegrees(45));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(1.2, Rotation2d.fromDegrees(50));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(1.4, Rotation2d.fromDegrees(54));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(1.6, Rotation2d.fromDegrees(57));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(1.8, Rotation2d.fromDegrees(60));
+            SLOPE_TO_SHOOTER_ROTATION_MAP.put(2.0, Rotation2d.fromDegrees(63));
+        }
+
+        public static final int
+                FLYWHEEL_LEFT_ID = 15,
+                FLYWHEEL_RIGHT_ID = 16,
+                PIVOT_ID = 1,
+                PIVOT_CAN_CODER = 22,
+                KICKER_ID = 8;
+        public static final double FLYWHEEL_MINIMUM_READY_SPEED = 0.7;
+        public static final double PIVOT_ENCODER_OFFSET = 40.78;
+        public static final double FLYWHEEL_P = 0.001_159;
+        public static final double FLYWHEEL_FF = 0.000_065_955;
+        public static final double FLYWHEEL_RANGE_MIN = -1;
+        public static final double FLYWHEEL_RANGE_MAX = 1;
+        public static final double PIVOT_P = 0.0001;//0.014_75;
+        public static final double PIVOT_FF = 0.000;//0.001;
+        public static final double PIVOT_RANGE_MIN = -0.3;
+        public static final double PIVOT_RANGE_MAX = 0.3;
+
+        public static final double FLYWHEEL_SPEED = 0.7;
+        /** In seconds */
+        public static final double SHOOTING_DELAY = 0.5;
+        /** In seconds */
+        public static final double POST_SHOOTING_DELAY = 0.4;
+
+        public static final double NOTE_RELEASE_VELOCITY = 0.5; //todo: CONFIGURE
+        public static final Translation3d TARGET_POSITION = new Translation3d();
+
     }
 
     public static final class Swerve {
-        public static final int PIGEON_ID = 15;
+        public static final int PIGEON_ID = 30;
         public static final boolean INVERT_GYRO = false; // Always ensure Gyro is CCW+ CW-
 
         public static final COTSFalconSwerveConstants CHOSEN_MODULE =
@@ -57,7 +145,7 @@ public final class Constants {
         public static final double WHEEL_BASE = 0.615;
         public static final double WHEEL_CIRCUMFERENCE = CHOSEN_MODULE.wheelCircumference;
 
-        /* Swerve Kinematics
+        /* SwerveSubsystem Kinematics
          * No need to ever change this unless you are not doing a traditional rectangular/square 4 module swerve */
         public static final SwerveDriveKinematics SWERVE_KINEMATICS = new SwerveDriveKinematics(
                 new Translation2d(WHEEL_BASE / 2.0, TRACK_WIDTH / 2.0),
@@ -76,7 +164,7 @@ public final class Constants {
         /* Angle Encoder Invert */
         public static final boolean CAN_CODER_INVERT = CHOSEN_MODULE.canCoderInvert;
 
-        /* Swerve Current Limiting */
+        /* SwerveSubsystem Current Limiting */
         public static final int ANGLE_CONTINUOUS_CURRENT_LIMIT = 25;
         public static final int ANGLE_PEAK_CURRENT_LIMIT = 40;
         public static final double ANGLE_PEAK_CURRENT_DURATION = 0.1;
@@ -110,7 +198,7 @@ public final class Constants {
         public static final double DRIVE_KV = 0; //(1.51 / 12);
         public static final double DRIVE_KA = 0; // (0.27 / 12);
 
-        /* Swerve Profiling Values */
+        /* SwerveSubsystem Profiling Values */
         /**
          * Meters per Second
          */
@@ -126,50 +214,50 @@ public final class Constants {
 
         /* Module Specific Constants */
         /* Front Left Module - Module 0 */
-        public static final class Mod0 {
-            public static final int DRIVE_MOTOR_ID = 7;
-            public static final int ANGLE_MOTOR_ID = 8;
-            public static final int CAN_CODER_ID = 9;
-            public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees(181.56);
+        public static final class Module0 {
+            public static final int DRIVE_MOTOR_ID = 14;
+            public static final int ANGLE_MOTOR_ID = 11;
+            public static final int CAN_CODER_ID = 18;
+            public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees(243.55);
             public static final SwerveModuleConstants CONSTANTS =
                     new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
         }
 
         /* Front Right Module - Module 1 */
-        public static final class Mod1 {
-            public static final int DRIVE_MOTOR_ID = 1;
-            public static final int ANGLE_MOTOR_ID = 2;
-            public static final int CAN_CODER_ID = 3;
+        public static final class Module1 {
+            public static final int DRIVE_MOTOR_ID = 3;
+            public static final int ANGLE_MOTOR_ID = 10;
+            public static final int CAN_CODER_ID = 20;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees(100.45);//281.51);
             public static final SwerveModuleConstants CONSTANTS =
                     new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
         }
 
         /* Back Left Module - Module 2 */
-        public static final class Mod2 {
-            public static final int DRIVE_MOTOR_ID = 10;
-            public static final int ANGLE_MOTOR_ID = 11;
-            public static final int CAN_CODER_ID = 12;
+        public static final class Module2 {
+            public static final int DRIVE_MOTOR_ID = 13;
+            public static final int ANGLE_MOTOR_ID = 6;
+            public static final int CAN_CODER_ID = 19;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees(194.23);//196.78);
             public static final SwerveModuleConstants CONSTANTS =
                     new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
         }
 
         /* Back Right Module - Module 3 */
-        public static final class Mod3 {
-            public static final int DRIVE_MOTOR_ID = 4;
-            public static final int ANGLE_MOTOR_ID = 5;
-            public static final int CAN_CODER_ID = 6;
+        public static final class Module3 {
+            public static final int DRIVE_MOTOR_ID = 2;
+            public static final int ANGLE_MOTOR_ID = 9;
+            public static final int CAN_CODER_ID = 21;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees(113.46);
             public static final SwerveModuleConstants CONSTANTS =
                     new SwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CAN_CODER_ID, ANGLE_OFFSET);
         }
 
-        public static boolean ANGLE_INVERT = true;
-        public static double VOLTAGE_COMP = 12.0;
+        public static final boolean ANGLE_INVERT = true;
+        public static final double VOLTAGE_COMP = 12.0;
         public static final double ANGLE_CONVERSION_FACTOR = 360.0 / ANGLE_GEAR_RATIO;
 
-        public static HolonomicPathFollowerConfig holomonicPathFollowerConfig = new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+        public static final HolonomicPathFollowerConfig holomonicPathFollowerConfig = new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
                 new PIDConstants(1.10, 0.0, 0.0), // Translation PID constants
                 new PIDConstants(1.18, 0.0, 0.0), // Rotation PID constants
                 Constants.Swerve.MAX_SPEED, // Max module speed, in m/s
