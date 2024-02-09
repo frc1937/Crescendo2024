d733f8d1bac0c54a54c6442e10e590d909236b3b
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-02-09 11:22:11 +0200

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,18 +7,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.IntakeConstants.INTAKE_MOTOR_ID;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private final WPI_TalonSRX talonSRX;
+    private final WPI_TalonSRX talonSRXIntake;
 
     public IntakeSubsystem() {
-        talonSRX = new WPI_TalonSRX(INTAKE_MOTOR_ID);
-        talonSRX.setInverted(true);
+        talonSRXIntake = new WPI_TalonSRX(INTAKE_MOTOR_ID);
+        talonSRXIntake.setInverted(true);
     }
 
     public void setSpeedPercentage(double percentage) {
-        talonSRX.set(ControlMode.PercentOutput, percentage);
+        talonSRXIntake.set(ControlMode.PercentOutput, percentage);
     }
 
     public void stopMotor() {
-        talonSRX.stopMotor();
+        talonSRXIntake.stopMotor();
     }
 }
