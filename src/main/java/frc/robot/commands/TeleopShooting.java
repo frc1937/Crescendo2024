8d86aa315782d96e46915e4826e22624cb2e7bd1
Amit Goren <animalfarm@mailfence.com>
2024-02-26 13:39:48 +0200

    fix: enable shooting even without NOTEs

--- src/main/java/frc/robot/commands/TeleopShooting.java
+++ src/main/java/frc/robot/commands/TeleopShooting.java
@@ -142,7 +142,7 @@ public class TeleopShooting extends SequentialCommandGroup {
 
             SmartDashboard.putBooleanArray("flywheels | pitch | yaw | slope", new boolean[]{flywheelsReady, pitchReady, yawReady, slopeViable});
 
-            return readyToKick || !shooter.doesSeeNote();
+            return readyToKick;
         }
 
         @Override
