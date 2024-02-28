740751dfc9531f2d3d495920fd497232e0919b27
Amit Goren <animalfarm@mailfence.com>
2024-02-28 12:32:25 +0200

    fix(predefined-shooting): increase the stage shooting power

--- src/main/java/frc/robot/util/ShootingStates.java
+++ src/main/java/frc/robot/util/ShootingStates.java
@@ -5,7 +5,7 @@ public enum ShootingStates {
   //  AMP(70, 0.2, 0.6),
     SPEAKER_FRONT(76, 0.72, 0.9), //WORKING
     SPEAKER_BACK(137, 0.69, 0.7), //WORK
-    STAGE_FRONT(60, 0.88, 0.98);
+    STAGE_FRONT(62, 0.93, 0.98);
 
     private final double angle, speedPercentage, rpmProportion;
     ShootingStates(double angle, double speedPercentage, double rpmProportion) {
