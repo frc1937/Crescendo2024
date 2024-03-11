604bd842b8ed1fcc995f9a85d2639f1a089b3f42
Wihy <105944317+itsWihy@users.noreply.github.com>
2024-03-11 23:25:13 +0200

    fix(pitch): technically working doing pitch

--- src/main/java/frc/robot/Pitch.java
+++ src/main/java/frc/robot/Pitch.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -18,7 +17,6 @@ import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants.ShootingConstants.FlywheelControlConstants;
 
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.DegreesPerSecond;
@@ -35,13 +33,13 @@ public class Pitch {
     private final CANSparkFlex motor;
     private final CANCoder encoder;
     private final ArmFeedforward feedforward;
-    private final TrapezoidProfile motionProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints
-            (0.1, 0.1));
-    private final TrapezoidProfile.State goal = new TrapezoidProfile.State();
-    private boolean goalIsSetpoint = false;
+    private final double goal = 0.7;
+//    private final TrapezoidProfile.State goal = new TrapezoidProfile.State(0.7, 0);
+//    private boolean goalIsSetpoint = false;
 
     public Pitch(int motorId, double p, double d, double s, double g, double v) {
-        feedback = new PIDController(p, 0, d);
+        feedback = new PIDController(p/5, 0, d/5);
+//        velocityController = new PIDController(2, 0, 0);
         feedforward = new ArmFeedforward(s, g, v);
 
         motor = new CANSparkFlex(motorId, MotorType.kBrushless);
@@ -53,47 +51,87 @@ public class Pitch {
 
         feedback.setTolerance(PIVOT_TOLERANCE);
 
-        // WARNING FIXME XXX TODO Shout at Uriel
-        // feedback.setTolerance(FlywheelControlConstants.TOLERANCE);
-
         encoder = new CANCoder(PIVOT_CAN_CODER);
         encoder.configFactoryDefault();
         encoder.configSensorDirection(true);
+
+//        SmartDashboard.putNumber("pitch/P-value", 1);
+//        SmartDashboard.putNumber("pitch/D-value", 0);
+//        SmartDashboard.putNumber("pitch/goal-value", 0);
     }
 
     public void periodic() {
+//        velocityController = new PIDController(SmartDashboard.getNumber("pitch/P-value", 0)
+//                , 0, SmartDashboard.getNumber("pitch/D-value", 0));
+//
+//        SmartDashboard.putNumber("GOAL Position [deg]", goal.position);
+//        SmartDashboard.putNumber("GOAL Current Deg [deg]", getPitch().getDegrees());
+
         // Calculate the position and velocity setpoints
 
-        TrapezoidProfile.State setpoint = motionProfile.calculate(0.02, getCurrentState(), goal);
-        goalIsSetpoint = goal.equals(setpoint);
+        //TrapezoidProfile.State setpoint = motionProfile.calculate(0.02, getCurrentState(), goal);
+        //goalIsSetpoint = goal.equals(setpoint);
+//        goalIsSetpoint = false;
 
-        setpoint.velocity = MathUtil.clamp(setpoint.velocity, -0.1, 0.1);
+//        double velocitySetpoint = 0;
+//
+//        if (!feedback.atSetpoint()) {
+//            velocitySetpoint = velocityController.calculate(getCurrentState().position, goal.position);
+//            velocitySetpoint = MathUtil.clamp(velocitySetpoint, -0.8, 0.8);
+//        }
+//        double velocitySetpoint = MathUtil.clamp((goal.position - getCurrentState().position) *
+//                SmartDashboard.getNumber("pitch/P-value", 2), -0.2, 0.2);
 
-        SmartDashboard.putNumber("Setpoint Position [deg]", setpoint.position);
-        SmartDashboard.putNumber("Setpoint Velocity [RadPS]", setpoint.velocity);
+//        SmartDashboard.putNumber("Setpoint Position [deg]", setpoint.position);
+//        SmartDashboard.putNumber("Setpoint Velocity [RadPS]", velocitySetpoint);
 
         // Derive the acceleration setpoint from the current and future velocity
-        double accelerationRadPerSecSquared = (setpoint.velocity - getCurrentVelocity().in(RadiansPerSecond)) / 0.02;
+//        double accelerationRadPerSecSquared = (velocitySetpoint - getCurrentVelocity().in(RadiansPerSecond)) / 0.02;
 
         // Control the motor
-        double velocityFeedforward = feedforward.calculate(setpoint.position, setpoint.velocity, accelerationRadPerSecSquared);
-        double positionFeedback = feedback.calculate(getPitch().getRotations(), Units.radiansToRotations(setpoint.position));
-        motor.setVoltage(velocityFeedforward + positionFeedback);
+        double voltage;
+
+        double error = goal - getCurrentState().position;
+        if (error < -0.1) {
+            voltage = feedforward.calculate(getCurrentState().position, -0.6, 0);
+        } else if (error > 0.1) {
+            voltage = feedforward.calculate(getCurrentState().position, 0.6, 0);
+        } else {
+            voltage = feedback.calculate(getPitch().getRotations(), Units.radiansToRotations(goal));
+        }
+//        if (Math.abs(getCurrentState().position - goal) > 0.1)
+//        double velocityFeedforward = feedforward.calculate(getCurrentState().position, velocitySetpoint, 0);
+
+//        if (goalIsSetpoint)
+//        double positionFeedback = feedback.calculate(getPitch().getRotations(), Units.radiansToRotations(setpoint.position));
+
+//        SmartDashboard.putNumber("FeedForwardVel [velocity units LOL]", velocityFeedforward);
+//        SmartDashboard.putNumber("FeedBack [position in rotations]", positionFeedback);
+//        SmartDashboard.putBoolean("FeedLoopHas Reached Goal!! [BOOL]", atGoal());
+
+        motor.setVoltage(voltage);
     }
 
     public void setGoal(Measure<Angle> position, Measure<Velocity<Angle>> velocity) {
-        goal.position = position.in(Radians);
-        goal.velocity = velocity.in(RadiansPerSecond);
+//        goal.position = position.in(Radians);
+//        goal.velocity = velocity.in(RadiansPerSecond);
+
+        SmartDashboard.putNumber("Position Goal [deg]", position.in(Degrees));
+        SmartDashboard.putNumber("Position Velocity [RadPS]", velocity.in(RadiansPerSecond));
+        //todo: check setpoint, velocity and gaol
     }
 
     @Deprecated
     public void setPosition(Rotation2d rotation2d) {
-        setGoal(Radians.of(rotation2d.getRadians()), RadiansPerSecond.of(0));
+        setGoal(Radians.of(
+                SmartDashboard.getNumber("pitch/goal-value", 0)
+//                rotation2d.getRadians()
+        ), RadiansPerSecond.of(0));
     }
 
     public boolean atGoal() {
         // WARNING: this does not check whether the velocity goal was reached
-        return goalIsSetpoint && feedback.atSetpoint();
+        return /*goalIsSetpoint && */feedback.atSetpoint();
     }
 
     @Deprecated
@@ -120,7 +158,7 @@ public class Pitch {
         motor.stopMotor();
     }
 
-    public TrapezoidProfile.State getCurrentState() {
+    private TrapezoidProfile.State getCurrentState() {
         return new TrapezoidProfile.State(getPitch().getRadians(), getCurrentVelocity().in(RadiansPerSecond));
     }
 }
